2c9dab6f7c6eeb4e770d384bfdb0748ecdfd4628
vnikolaou <vnikolaou12@gmail.com>
2018-05-25 11:34:56 +0300

    init project

---
+++ src/main/java/com/pionware/lib/Utils.java
@@ -0,0 +1,11 @@
+package com.pionware.lib;
+
+public class Utils {
+	public static long sum(long x, long y) {
+		return x + y;
+	}
+	
+	public static long multiply(long x, long y) {
+		return x * y;
+	}	
+}
